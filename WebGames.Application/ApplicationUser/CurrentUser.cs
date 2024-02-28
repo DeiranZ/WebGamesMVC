32fc88c0a25395cab85b89e27fa6569f019c8b43
Jakub Wabik <40063902+DeiranZ@users.noreply.github.com>
2024-02-28 17:42:08 +0100

    Account Level rendering

--- WebGames.Application/ApplicationUser/CurrentUser.cs
+++ WebGames.Application/ApplicationUser/CurrentUser.cs
@@ -2,16 +2,18 @@
 {
 	public class CurrentUser
 	{
-		public CurrentUser(string id, string email, IEnumerable<string> roles)
+		public CurrentUser(string id, string email, IEnumerable<string> roles, uint level)
 		{
 			Id = id;
 			Email = email;
 			Roles = roles;
+			Level = level;
 		}
 
 		public string Id { get; set; }
 		public string Email { get; set; }
 		public IEnumerable<string> Roles { get; set; }
+		public uint Level { get; set; }
 
 		public bool IsInRole(string role)
 		{
--- WebGames.Application/ApplicationUser/UserContext.cs
+++ WebGames.Application/ApplicationUser/UserContext.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
+using WebGames.Domain.ApplicationUser;
 
 namespace WebGames.Application.ApplicationUser
 {
 	public class UserContext : IUserContext
 	{
 		private readonly IHttpContextAccessor httpContextAccessor;
+		private readonly UserManager<Domain.ApplicationUser.ApplicationUser> userManager;
 
-		public UserContext(IHttpContextAccessor httpContextAccessor)
+		public UserContext(IHttpContextAccessor httpContextAccessor, UserManager<Domain.ApplicationUser.ApplicationUser> userManager)
 		{
 			this.httpContextAccessor = httpContextAccessor;
+			this.userManager = userManager;
 		}
 
 		public CurrentUser? GetCurrentUser()
@@ -29,8 +33,9 @@ namespace WebGames.Application.ApplicationUser
 			var id = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
 			var email = user.FindFirst(c => c.Type == ClaimTypes.Email)!.Value;
 			var roles = user.Claims.Where(c => c.Type == ClaimTypes.Role)!.Select(c => c.Value);
+			var level = userManager.GetUserAsync(user).Result!.Level;
 
-			return new CurrentUser(id, email, roles);
+			return new CurrentUser(id, email, roles, level);
 		}
 	}
 }
