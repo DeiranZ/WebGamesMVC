8653d3d3a82f970000eb17334b682dfef7758575
Jakub Wabik <40063902+DeiranZ@users.noreply.github.com>
2024-03-04 12:29:06 +0100

    Validation Testing

---
+++ WebGames.Application.Tests/Game/Commands/CreateGame/CreateGameCommandValidatorTests.cs
@@ -0,0 +1,92 @@
+﻿using Xunit;
+using Moq;
+using FluentValidation.TestHelper;
+using WebGames.Application.Genre.Commands.CreateGenre;
+using WebGames.Domain.Interfaces;
+
+namespace WebGames.Application.Game.Commands.CreateGame.Tests
+{
+    public class CreateGameCommandValidatorTests
+    {
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldNotHaveValidationError()
+        {
+            // arrange
+
+            var repositoryMock = new Mock<IGameRepository>();
+
+            var validator = new CreateGameCommandValidator(repositoryMock.Object);
+
+            var command = new CreateGameCommand()
+            {
+                Name = "Some Game Name",
+                Description = "This is the description",
+                Source = "some-link.com/home"
+            };
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            // assert
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact()]
+        public void Validate_WithInvalidCommand_ShouldHaveValidationErrors()
+        {
+            // arrange
+
+            var repositoryMock = new Mock<IGameRepository>();
+
+            var validator = new CreateGameCommandValidator(repositoryMock.Object);
+
+            var command = new CreateGameCommand()
+            {
+                Name = "",
+                Description = new string('\t', 32768),
+                Source = new string('\t', 32768)
+            };
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            // assert
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+            result.ShouldHaveValidationErrorFor(x => x.Description);
+            result.ShouldHaveValidationErrorFor(x => x.Source);
+        }
+
+        [Fact()]
+        public void Validate_WithExistingName_ShouldHaveValidationError()
+        {
+            // arrange
+
+            var repositoryMock = new Mock<IGameRepository>();
+
+
+            var validator = new CreateGameCommandValidator(repositoryMock.Object);
+
+            var command = new CreateGameCommand()
+            {
+                Name = "",
+                Description = new string('\t', 32768),
+                Source = new string('\t', 32768)
+            };
+
+            repositoryMock.Setup(x => x.GetByName(command.Name).Result)
+                .Returns(new Domain.Entities.Game());
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            // assert
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+    }
+}
---
+++ WebGames.Application.Tests/Genre/Commands/CreateGenre/CreateGenreCommandValidatorTests.cs
@@ -0,0 +1,88 @@
+﻿using Xunit;
+using Moq;
+using WebGames.Domain.Interfaces;
+using FluentValidation.TestHelper;
+using WebGames.Application.Game.Commands.CreateGame;
+
+namespace WebGames.Application.Genre.Commands.CreateGenre.Tests
+{
+    public class CreateGenreCommandValidatorTests
+    {
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldNotHaveValidationError()
+        {
+            // arrange
+
+            var repositoryMock = new Mock<IGenreRepository>();
+
+            var validator = new CreateGenreCommandValidator(repositoryMock.Object);
+
+            var command = new CreateGenreCommand()
+            {
+                Name = "Match 3",
+                Description = "Match three tiles together!"
+            };
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            // assert
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact()]
+        public void Validate_WithInvalidCommand_ShouldHaveValidationErrors()
+        {
+            // arrange
+
+            var repositoryMock = new Mock<IGenreRepository>();
+
+            var validator = new CreateGenreCommandValidator(repositoryMock.Object);
+
+            var command = new CreateGenreCommand()
+            {
+                Name = "",
+                Description = new string('\t', 32768)
+            };
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            // assert
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+            result.ShouldHaveValidationErrorFor(x => x.Description);
+        }
+
+        [Fact()]
+        public void Validate_WithExistingName_ShouldHaveValidationError()
+        {
+            // arrange
+
+            var repositoryMock = new Mock<IGenreRepository>();
+
+
+            var validator = new CreateGenreCommandValidator(repositoryMock.Object);
+
+            var command = new CreateGenreCommand()
+            {
+                Name = "",
+                Description = new string('\t', 32768),
+            };
+
+            repositoryMock.Setup(x => x.GetByName(command.Name).Result)
+                .Returns(new Domain.Entities.Genre());
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            // assert
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+    }
+}
