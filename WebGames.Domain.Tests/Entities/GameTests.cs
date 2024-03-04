b3bcc24d6f67b39726952b97ecbc96cbe1ca2142
Jakub Wabik <40063902+DeiranZ@users.noreply.github.com>
2024-03-04 10:43:45 +0100

    Domain tests

---
+++ WebGames.Domain.Tests/Entities/GameTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace WebGames.Domain.Entities.Tests
+{
+    public class GameTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            // arrange
+
+            var game = new Game();
+            game.Name = "Cool Game";
+
+            // act
+
+            game.EncodeName();
+
+            // assert
+
+            game.EncodedName.Should().Be("cool-game");
+        }
+
+        [Fact()]
+        public void EncodeName_ShouldThrowException_WhenNameIsNull()
+        {
+            // arrange
+
+            var game = new Game();
+
+            // act
+
+            Action action = () => game.EncodeName();
+
+            // assert
+
+            action.Invoking(a => a.Invoke())
+                .Should().Throw<NullReferenceException>();
+        }
+    }
+}
---
+++ WebGames.Domain.Tests/Entities/GenreTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace WebGames.Domain.Entities.Tests
+{
+    public class GenreTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            // arrange
+
+            var genre = new Genre();
+            genre.Name = "Some Genre";
+
+            // act
+
+            genre.EncodeName();
+
+            // assert
+
+            genre.EncodedName.Should().Be("some-genre");
+        }
+
+        [Fact()]
+        public void EncodeName_ShouldThrowException_WhenNameIsNull()
+        {
+            // arrange
+
+            var genre = new Genre();
+
+            // act
+
+            Action action = () => genre.EncodeName();
+
+            // assert
+
+            action.Invoking(a => a.Invoke())
+                .Should().Throw<NullReferenceException>();
+        }
+    }
+}
