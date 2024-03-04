e6aa782e5fe64bf1c23615feb42d4a3470cfb586
Jakub Wabik <40063902+DeiranZ@users.noreply.github.com>
2024-03-04 13:16:29 +0100

    Create Commands Tests

---
+++ WebGames.Application.Tests/Game/Commands/CreateGame/CreateGameCommandHandlerTests.cs
@@ -0,0 +1,87 @@
+﻿using Xunit;
+using WebGames.Application.Game.Commands.CreateGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Moq;
+using WebGames.Application.ApplicationUser;
+using WebGames.Application.Genre.Commands.CreateGenre;
+using WebGames.Domain.Interfaces;
+using WebGames.Application.Mappings;
+
+namespace WebGames.Application.Game.Commands.CreateGame.Tests
+{
+    public class CreateGameCommandHandlerTests
+    {
+        [Fact()]
+        public async Task Handle_CreateGame_WhenUserIsAuthorized()
+        {
+            // arrange
+
+            var command = new CreateGameCommand()
+            {
+                Name = "Cool Game",
+                Description = "Some name",
+                EncodedName = "cool-game",
+                Source = "source.com"
+            };
+
+            var userContextMock = new Mock<IUserContext>();
+
+            userContextMock.Setup(c => c.GetCurrentUser())
+                .Returns(new CurrentUser("1", "some@email.com", new List<string>() { "Admin" }, 1));
+
+            var gameRepositoryMock = new Mock<IGameRepository>();
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new GameMappingProfile()));
+            var mapper = configuration.CreateMapper();
+
+            var handler = new CreateGameCommandHandler(gameRepositoryMock.Object, mapper, userContextMock.Object);
+
+            // act
+
+            await handler.Handle(command, CancellationToken.None);
+
+            // assert
+
+            gameRepositoryMock.Verify(m => m.Create(It.IsAny<Domain.Entities.Game>()), Times.Once);
+        }
+
+        [Fact()]
+        public async Task Handle_DontCreateGame_WhenUserIsNotAuthorized()
+        {
+            // arrange
+
+            var command = new CreateGameCommand()
+            {
+                Name = "Cool Game",
+                Description = "Some name",
+                EncodedName = "cool-game",
+                Source = "source.com"
+            };
+
+            var userContextMock = new Mock<IUserContext>();
+
+            userContextMock.Setup(c => c.GetCurrentUser())
+                .Returns(new CurrentUser("1", "some@email.com", new List<string>() { "User" }, 1));
+
+            var gameRepositoryMock = new Mock<IGameRepository>();
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new GameMappingProfile()));
+            var mapper = configuration.CreateMapper();
+
+            var handler = new CreateGameCommandHandler(gameRepositoryMock.Object, mapper, userContextMock.Object);
+
+            // act
+
+            await handler.Handle(command, CancellationToken.None);
+
+            // assert
+
+            gameRepositoryMock.Verify(m => m.Create(It.IsAny<Domain.Entities.Game>()), Times.Never);
+        }
+    }
+}
---
+++ WebGames.Application.Tests/Genre/Commands/CreateGenre/CreateGenreCommandHandlerTests.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Moq;
+using WebGames.Application.ApplicationUser;
+using WebGames.Application.Mappings;
+using WebGames.Domain.Interfaces;
+using Xunit;
+
+namespace WebGames.Application.Genre.Commands.CreateGenre.Tests
+{
+    public class CreateGenreCommandHandlerTests
+    {
+        [Fact()]
+        public async Task Handle_CreateGenre_WhenUserIsAuthorized()
+        {
+            // arrange
+
+            var command = new CreateGenreCommand()
+            {
+                Name = "Match 3",
+                Description = "Match three tiles together!",
+                EncodedName = "match-3"
+            };
+
+            var userContextMock = new Mock<IUserContext>();
+
+            userContextMock.Setup(c => c.GetCurrentUser())
+                .Returns(new CurrentUser("1", "some@email.com", new List<string>() { "Admin" }, 1));
+
+            var genreRepositoryMock = new Mock<IGenreRepository>();
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new GenreMappingProfile()));
+            var mapper = configuration.CreateMapper();
+
+            var handler = new CreateGenreCommandHandler(genreRepositoryMock.Object, mapper, userContextMock.Object);
+
+            // act
+
+            await handler.Handle(command, CancellationToken.None);
+            
+            // assert
+
+            genreRepositoryMock.Verify(m => m.Create(It.IsAny<Domain.Entities.Genre>()), Times.Once);
+        }
+
+        [Fact()]
+        public async Task Handle_DontCreateGenre_WhenUserIsNotAuthorized()
+        {
+            // arrange
+
+            var command = new CreateGenreCommand()
+            {
+                Name = "Match 3",
+                Description = "Match three tiles together!",
+                EncodedName = "match-3"
+            };
+
+            var userContextMock = new Mock<IUserContext>();
+
+            userContextMock.Setup(c => c.GetCurrentUser())
+                .Returns(new CurrentUser("1", "some@email.com", new List<string>() { "User" }, 1));
+
+            var genreRepositoryMock = new Mock<IGenreRepository>();
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new GenreMappingProfile()));
+            var mapper = configuration.CreateMapper();
+
+            var handler = new CreateGenreCommandHandler(genreRepositoryMock.Object, mapper, userContextMock.Object);
+
+            // act
+
+            await handler.Handle(command, CancellationToken.None);
+
+            // assert
+
+            genreRepositoryMock.Verify(m => m.Create(It.IsAny<Domain.Entities.Genre>()), Times.Never);
+        }
+    }
+}
