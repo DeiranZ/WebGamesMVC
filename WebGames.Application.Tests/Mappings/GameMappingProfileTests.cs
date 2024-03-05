cb053b59616a923a028093707a074d4dbda69013
Jakub Wabik <40063902+DeiranZ@users.noreply.github.com>
2024-03-05 15:39:01 +0100

    Mapper tests

---
+++ WebGames.Application.Tests/Mappings/GameMappingProfileTests.cs
@@ -0,0 +1,73 @@
+﻿using Xunit;
+using AutoMapper;
+using WebGames.Application.Game;
+using FluentAssertions;
+
+namespace WebGames.Application.Mappings.Tests
+{
+    public class GameMappingProfileTests
+    {
+        [Fact()]
+        public void MappingProfile_ShouldMapGameDtoToGame()
+        {
+            // arrange
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile<GameMappingProfile>());
+            var mapper = configuration.CreateMapper();
+
+            var dto = new GameDto
+            {
+                Name = "Test",
+                Description = "TestDesc",
+                Source = "some.source",
+                ImageSource = "image.source",
+                EncodedName = "test"
+            };
+
+            // act
+
+            var result = mapper.Map<Domain.Entities.Game>(dto);
+
+            // assert
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(dto.Name);
+            result.Description.Should().Be(dto.Description);
+            result.Source.Should().Be(dto.Source);
+            result.ImageSource.Should().Be(dto.ImageSource);
+            result.EncodedName.Should().Be(dto.EncodedName);
+        }
+
+        [Fact()]
+        public void MappingProfile_ShouldMapGameToGameDto()
+        {
+            // arrange
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile<GameMappingProfile>());
+            var mapper = configuration.CreateMapper();
+
+            var game = new Domain.Entities.Game
+            {
+                Name = "Test",
+                Description = "TestDesc",
+                Source = "some.source",
+                ImageSource = "image.source",
+            };
+
+            game.EncodeName();
+
+            // act
+
+            var result = mapper.Map<GameDto>(game);
+
+            // assert
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(game.Name);
+            result.Description.Should().Be(game.Description);
+            result.Source.Should().Be(game.Source);
+            result.ImageSource.Should().Be(game.ImageSource);
+            result.EncodedName.Should().Be(game.EncodedName);
+        }
+    }
+}
---
+++ WebGames.Application.Tests/Mappings/GenreMappingProfileTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using AutoMapper;
+using WebGames.Application.Mappings;
+using WebGames.Application.Genre;
+using WebGames.Domain.Entities;
+using FluentAssertions;
+
+namespace WebGenres.Application.Mappings.Tests
+{
+    public class GenreMappingProfileTests
+    {
+        [Fact()]
+        public void MappingProfile_ShouldMapGenreDtoToGenre()
+        {
+            // arrange
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile<GenreMappingProfile>());
+            var mapper = configuration.CreateMapper();
+
+            var dto = new GenreDto
+            {
+                Name = "Test",
+                Description = "TestDesc",
+                EncodedName = "test"
+            };
+
+            // act
+
+            var result = mapper.Map<Genre>(dto);
+
+            // assert
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(dto.Name);
+            result.Description.Should().Be(dto.Description);
+            result.EncodedName.Should().Be(dto.EncodedName);
+        }
+
+        [Fact()]
+        public void MappingProfile_ShouldMapGenreToGenreDto()
+        {
+            // arrange
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile<GenreMappingProfile>());
+            var mapper = configuration.CreateMapper();
+
+            var genre = new Genre
+            {
+                Name = "Test",
+                Description = "TestDesc",
+            };
+
+            genre.EncodeName();
+
+            // act
+
+            var result = mapper.Map<GenreDto>(genre);
+
+            // assert
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(genre.Name);
+            result.Description.Should().Be(genre.Description);
+            result.EncodedName.Should().Be(genre.EncodedName);
+        }
+    }
+}
