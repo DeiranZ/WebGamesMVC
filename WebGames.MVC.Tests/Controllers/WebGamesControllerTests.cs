3b1bb2b0290ff509d2ebd1ebd285c48f96a23214
Jakub Wabik <40063902+DeiranZ@users.noreply.github.com>
2024-03-05 18:16:41 +0100

    WebGames/Edit Test

--- WebGames.MVC.Tests/Controllers/WebGamesControllerTests.cs
+++ WebGames.MVC.Tests/Controllers/WebGamesControllerTests.cs
@@ -7,6 +7,10 @@ using WebGames.Application.Game.Queries.GetAllGames;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using FluentAssertions;
+using WebGames.Application.Genre;
+using WebGames.Application.Genre.Queries.GetAllGenres;
+using WebGames.Application.Game.Queries.GetGameByEncodedName;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebGames.MVC.Controllers.Tests
 {
@@ -25,7 +29,7 @@ namespace WebGames.MVC.Controllers.Tests
             // arrange
 
             var games = new List<GameDto>
-            { 
+            {
                 new GameDto()
                 {
                     Name = "Game 1"
@@ -48,7 +52,7 @@ namespace WebGames.MVC.Controllers.Tests
                 .ReturnsAsync(games);
 
             var client = factory
-                .WithWebHostBuilder(builder => 
+                .WithWebHostBuilder(builder =>
                     builder.ConfigureTestServices(services => services.AddScoped(_ => mediatorMock.Object)))
                 .CreateClient();
 
@@ -98,6 +102,79 @@ namespace WebGames.MVC.Controllers.Tests
 
             content.Should().Contain("<h1 class=\"mb-12\">All Games</h1>")
                 .And.Contain("<h2>There are no games yet.</h2>");
+        }
+
+        [Fact()]
+        public async Task Edit_GenresShowLoadingSpinner_WithExpectedEncodedName()
+        {
+            // arrange
+
+            var genres = new List<GenreDto>
+            {
+                new GenreDto()
+                {
+                    Name = "Genre 1"
+                },
+
+                new GenreDto()
+                {
+                    Name = "Genre 2"
+                },
+
+                new GenreDto()
+                {
+                    Name = "Genre 3"
+                }
+            };
+
+            var game = new GameDto() { EncodedName = "test" };
+            
+            var mediatorMock = new Mock<IMediator>();
+
+            mediatorMock.Setup(x => x.Send(It.IsAny<GetAllGenresQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(genres);
+
+            mediatorMock.Setup(x => x.Send(It.IsAny<GetGameByEncodedNameQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(game);
+
+            var client = factory
+                .WithWebHostBuilder(builder =>
+                    builder.ConfigureTestServices(services =>
+                    {
+                        services.AddScoped(_ => mediatorMock.Object);
+                        
+                        var authorizationDescriptor = services.FirstOrDefault(d => d.ServiceType == typeof(IAuthorizationHandler));
+                        if (authorizationDescriptor != null)
+                            services.Remove(authorizationDescriptor);
+
+                        services.AddScoped<IAuthorizationHandler, TestAllowAnonymous>();
+                    }))
+                .CreateClient();
+
+            // act
+
+            var response = await client.GetAsync(game.EncodedName + "/edit");
+
+            // assert
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            content.Should().Contain($"<div id=\"genres\" class=\"row\" data-encoded-name=\"{game.EncodedName}\">")
+                .And.Contain("<div class=\"spinner-border m-5\" style=\"width: 3rem; height: 3rem;\" role=\"status\">")
+                .And.Contain("<span class=\"visually-hidden\">Loading...</span>");
+
+        }
+    }
+
+    public class TestAllowAnonymous : IAuthorizationHandler
+    {
+        public Task HandleAsync(AuthorizationHandlerContext context)
+        {
+            foreach (IAuthorizationRequirement requirement in context.PendingRequirements.ToList())
+                context.Succeed(requirement);
+            return Task.CompletedTask;
         }
     }
 }
